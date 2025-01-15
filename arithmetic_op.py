ac65f3935de00d7e6d01276064f3df1afc28d630
Francesco Dessi <fradessi21@gmail.com>
2025-01-15 18:53:38 +0100

    added arithmetic and boolean

---
+++ arithmetic_op.py
@@ -0,0 +1,5 @@
+print(2+6)
+print(20/5)
+print(22/5)
+print(22%5)
+print(2**3)
---
+++ bool_operators.py
@@ -0,0 +1,11 @@
+print(not True)
+print(not False)
+print(not 7)
+
+print(True or False)
+print(True and False)
+print(2 or 3 or 4)
+print(False or 0 or 4 or True)
+print(1 and 2 and 3)
+print (1 and 0.0 and 4)
+print(False or 0 or None)
